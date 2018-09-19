b9a7a848e3f1c57a68b80dad151b5ddab358835f
Balbuhhha <balbuhhha@gmail.com>
2018-09-19 23:16:30 +0200

    need help with conditions on mouseclick

--- mouse-click.js
+++ mouse-click.js
@@ -1,17 +1,18 @@
 var table = document.querySelector('tbody');
-var x = document.querySelectorAll('x');
+var availableCells = document.querySelectorAll('td:not(.x):not(.o)');
 
 //dodaj warunek, że komórka nie może być już z klasą x lub o
 table.addEventListener('click', function (event) {
+ // if (availableCells) {
   event.target.classList.add('x');
   drawItem('o');
-
+  })
   function drawNumberFromRange(size) {
     return Math.floor(Math.random() * size);
   }
 
   function drawCellFromSelector(selector) {
-    var availableCells = document.querySelectorAll(selector);
+    availableCells = document.querySelectorAll(selector);
     var randomIndex = drawNumberFromRange(availableCells.length);
     return availableCells[randomIndex];
   }
@@ -20,4 +21,3 @@ table.addEventListener('click', function (event) {
     var randomCell = drawCellFromSelector('td:not(.x):not(.o)');
     randomCell.classList.add(name);
   }
-})
