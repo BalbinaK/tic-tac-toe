5424411a75f530a8e083a2aa6933fd6d70076768
Balbuhhha <balbuhhha@gmail.com>
2018-09-19 23:03:01 +0200

    computer writes O randomly when player writes X

--- mouse-click.js
+++ mouse-click.js
@@ -1,9 +1,23 @@
-window.addEventListener('click', function(event) {
-    event.target.classList.add('x');
-    
-    //var clickedElement = event.target
-   // if (clickedElement.classList.contains('taken')) {
-   //   return;
-   // }
-   // clickedElement.classList.add('x');
+var table = document.querySelector('tbody');
+var x = document.querySelectorAll('x');
+
+//dodaj warunek, że komórka nie może być już z klasą x lub o
+table.addEventListener('click', function (event) {
+  event.target.classList.add('x');
+  drawItem('o');
+
+  function drawNumberFromRange(size) {
+    return Math.floor(Math.random() * size);
+  }
+
+  function drawCellFromSelector(selector) {
+    var availableCells = document.querySelectorAll(selector);
+    var randomIndex = drawNumberFromRange(availableCells.length);
+    return availableCells[randomIndex];
+  }
+
+  function drawItem(name) {
+    var randomCell = drawCellFromSelector('td:not(.x):not(.o)');
+    randomCell.classList.add(name);
+  }
 })
