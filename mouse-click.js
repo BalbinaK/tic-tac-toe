c7841fd99a52c7256d2ddc459bd9d3beafacf510
Balbuhhha <balbuhhha@gmail.com>
2018-09-20 13:58:13 +0200

    click works only on empty cells

--- mouse-click.js
+++ mouse-click.js
@@ -1,23 +1,29 @@
 var table = document.querySelector('tbody');
-var availableCells = document.querySelectorAll('td:not(.x):not(.o)');
 
-//dodaj warunek, że komórka nie może być już z klasą x lub o
+
+//dodaję warunek, że komórka nie może być już z klasą x lub o
 table.addEventListener('click', function (event) {
- // if (availableCells) {
-  event.target.classList.add('x');
-  drawItem('o');
-  })
-  function drawNumberFromRange(size) {
-    return Math.floor(Math.random() * size);
-  }
+  var availableCells = document.querySelectorAll('td:not(.x):not(.o)');
+  
+  if (event.target.classList.contains('x') || event.target.classList.contains('o')) {
+  return;
+}
 
-  function drawCellFromSelector(selector) {
-    availableCells = document.querySelectorAll(selector);
-    var randomIndex = drawNumberFromRange(availableCells.length);
-    return availableCells[randomIndex];
-  }
+event.target.classList.add('x');
+drawItem('o');
 
-  function drawItem(name) {
-    var randomCell = drawCellFromSelector('td:not(.x):not(.o)');
-    randomCell.classList.add(name);
-  }
+function drawNumberFromRange(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function drawCellFromSelector(selector) {
+  availableCells = document.querySelectorAll(selector);
+  var randomIndex = drawNumberFromRange(availableCells.length);
+  return availableCells[randomIndex];
+}
+
+function drawItem(name) {
+  var randomCell = drawCellFromSelector('td:not(.x):not(.o)');
+  randomCell.classList.add(name);
+}
+})
