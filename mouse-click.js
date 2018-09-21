ae31e684dc2fd738cfdaeeafd8855882f0cd7d59
Balbuhhha <balbuhhha@gmail.com>
2018-09-21 11:26:27 +0200

    win x works

--- mouse-click.js
+++ mouse-click.js
@@ -1,7 +1,13 @@
 var table = document.querySelector('tbody');
 var field0 = document.querySelector('.field0');
-  var field1 = document.querySelector('.field1');
-  var field2 = document.querySelector('.field2');
+var field1 = document.querySelector('.field1');
+var field2 = document.querySelector('.field2');
+var field3 = document.querySelector('.field3');
+var field4 = document.querySelector('.field4');
+var field5 = document.querySelector('.field5');
+var field6 = document.querySelector('.field6');
+var field7 = document.querySelector('.field7');
+var field8 = document.querySelector('.field8');
 
 //dodaję warunek, że komórka nie może być już z klasą x lub o
 table.addEventListener('click', function (event) {
@@ -34,6 +40,34 @@ table.addEventListener('click', function (event) {
 
 function checkResult(player) {
   if (field0.classList.contains(player) && field1.classList.contains(player) && field2.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field3.classList.contains(player) && field4.classList.contains(player) && field5.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field6.classList.contains(player) && field7.classList.contains(player) && field8.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field0.classList.contains(player) && field3.classList.contains(player) && field6.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field1.classList.contains(player) && field4.classList.contains(player) && field7.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field2.classList.contains(player) && field5.classList.contains(player) && field8.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field0.classList.contains(player) && field4.classList.contains(player) && field8.classList.contains(player)) {
+    console.log('player ' + player + ' wins!')
+    return true;
+  }
+  if (field2.classList.contains(player) && field4.classList.contains(player) && field6.classList.contains(player)) {
     console.log('player ' + player + ' wins!')
     return true;
   }
